76126be74dde6d6b939e843455df112eec6d11ce
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-19 14:15:23 -0700

    displays properly on page.
    Co-authored-by: m-wright92 <m-wright92@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$('#dinosaurs').click(function() {
+  let xhr = new XMLHttpRequest();
+  const url = `https://dinoipsum.com/api/?format=html&words=10&paragraphs=3`
+
+  xhr.open('GET',url, true);
+  xhr.onload = function() {
+    if (this.status === 200) {
+      document.querySelector('.container').innerHTML = this.response;
+    } else {
+      console.log('Where did all the dinosaurs go?');
+    }
+  };
+ xhr.send();
+})
+
